eccd003834e64ef2425c24f51df18599798b3082
Phan Ba Nhat Khuong <khuongphan275@gmail.com>
2026-09-16 14:41:03 +0700

    feat(service): add ProductService

--- slot3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/model/Product.java
+++ slot3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/model/Product.java
@@ -16,7 +16,7 @@ import java.math.BigDecimal;
 @Data
 public class Product {
     @Id
-    private String Id;
+    private String id;
     private String name;
     private String description;
     private BigDecimal price;
