e9b001b1eb4f3e805792482452aeea49f8f2f1ba
Phan Ba Nhat Khuong <khuongphan275@gmail.com>
2026-09-17 15:30:13 +0700

    test(service): add 2 unit tests for ProductServiceApplicationTests

--- slot3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/dto/ProductRequest.java
+++ slot3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/dto/ProductRequest.java
@@ -1,6 +1,9 @@
 package com.fudn.product_service.dto;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 
+@Builder
 public record ProductRequest(String id, String name, String description, BigDecimal price) {
 }
